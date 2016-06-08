435893a9acf5b498151c11e300488361dea0e7a0
Mirela Damyanova <mdamyanova181@gmail.com>
2016-06-08 19:37:28 +0300

    added solutions

---
+++ Programming Fundamentals/1.1Git and GitHub, Debugging and Troubleshooting/Non-WorkingDaysBetweenTwoDates/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace Non_WorkingDaysBetweenTwoDates
+{
+    public class HolidaysBetweenTwoDates
+    {
+        public static void Main()
+        {
+            var startDate = DateTime.ParseExact(Console.ReadLine(),
+              "d.M.yyyy", CultureInfo.InvariantCulture);
+            var endDate = DateTime.ParseExact(Console.ReadLine(),
+                "d.M.yyyy", CultureInfo.InvariantCulture);
+            var holidaysCount = 0;
+            for (var date = startDate; date <= endDate; date = date.AddDays(1.00))
+            {
+                if (date.DayOfWeek.Equals(DayOfWeek.Saturday) ||
+                    date.DayOfWeek.Equals(DayOfWeek.Sunday))
+                {
+                    holidaysCount++;
+                }
+            }
+
+            Console.WriteLine(holidaysCount);
+        }
+    }
+}
