5d29de5ca969912758ab5e8a852ade3c7dd9f4d7
Mirela Damyanova <mdamyanova181@gmail.com>
2016-07-27 16:16:04 +0300

    added solutions

---
+++ Software Technologies/Part IV - Java Web Development/1.1Java Basics-Syntax, Loops, Arrays, Classes and Objects/Problem04_SumNIntegers.java
@@ -0,0 +1,9 @@
+
+public class Problem04_SumNIntegers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Software Technologies/Part IV - Java Web Development/1.1Java Basics-Syntax, Loops, Arrays, Classes and Objects/Problem05_SymmetricNumbers.java
@@ -0,0 +1,9 @@
+
+public class Problem05_SymmetricNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
